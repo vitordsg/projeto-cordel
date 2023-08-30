b264efd9e0570e1c9f04361d5a9fa27ad22b10bd
Vitor Santos <vitinho6.6@hotmail.com>
2023-08-30 02:51:29 -0300

    Refazendo o projeto

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ const content = document.querySelector('.content')
 const section = document.querySelector('.exemplo')
 const img_container = document.querySelector('.imgContainer')
 const opacidade = document.querySelectorAll('.opacity')
-const border = document.querySelector('.border')
+const border = document.querySelectorAll('.border')
 
 let header_height = header.offsetHeight
 let section_height = section.offsetHeight
@@ -30,5 +30,16 @@ window.addEventListener('scroll' , () => {
     // content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`
     // img_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * - 50 + 50}px)`
 
-    border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`
+    let scrollSection = sectionY.top
+    border.forEach(element => {
+        
+        element.style.width = `${scroll / (scrollSection + section_height) * 0.5}%`
+        // if (scrollSection < -200) {
+        //     element.style.width = `${scroll / (scrollSection + section_height) * 30}%`;
+        // } 
+        // else {
+        //     element.style.width = `${scroll / (scrollSection + section_height) * 30}%`;
+        // }
+        console.log(scrollSection)
+    })
 })
